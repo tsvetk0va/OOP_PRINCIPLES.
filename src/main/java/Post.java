b949ee246c40becfae7133c4b93f402ba6624239
tsvetk0va <kknn2821@mail.ru>
2022-06-17 13:11:30 +0300

    post class modified

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,12 +1,12 @@
 public class Post {
-
+    public FormDate birthday;
     public String name;
     public String passsport;
     public String patronymic;
     public String phone;
     public String surname;
     public Boolean subscription;
-    public FormDate birthday;
+
 
 
 }
